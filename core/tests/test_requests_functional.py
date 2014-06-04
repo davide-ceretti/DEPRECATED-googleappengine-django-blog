8a0ebd1e7d2e17e25b72f1b3635a83af4c2e59b1
Davide Ceretti <dav.ceretti@gmail.com>
2014-06-04 22:19:45 +0100

    Add list of articles

--- core/tests/test_requests_functional.py
+++ core/tests/test_requests_functional.py
@@ -1,6 +1,6 @@
 from ndbtestcase import AppEngineTestCase
 
-from core.models import Blog
+from core.models import Blog, Article
 
 
 class TestArticleListPage(AppEngineTestCase):
@@ -9,13 +9,25 @@ class TestArticleListPage(AppEngineTestCase):
     """
     def setUp(self):
         Blog(title="my_blog_title").put()
-        self.resp = self.client.get('/')
 
     def test_correct_template_loaded(self):
+        resp = self.client.get('/')
         self.assertTemplateUsed(
             template_name="article_list.html",
-            response=self.resp
+            response=resp
         )
 
     def test_200_and_name_in_page(self):
-        self.assertContains(self.resp, "my_blog_title")
+        resp = self.client.get('/')
+        self.assertContains(resp, "my_blog_title")
+
+    def test_no_articles(self):
+        resp = self.client.get('/')
+        self.assertContains(resp, "This blog looks empty!")
+
+    def test_with_articles(self):
+        Article(title="title_article_one").put()
+        Article(title="title_article_two").put()
+        resp = self.client.get('/')
+        self.assertContains(resp, "title_article_one")
+        self.assertContains(resp, "title_article_two")
