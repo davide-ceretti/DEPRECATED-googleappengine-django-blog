11751c2d57fdf80b6dab5fb8aff217e717926c23
Davide Ceretti <dav.ceretti@gmail.com>
2014-06-04 18:33:28 +0100

    Reorganize tests in package

---
+++ core/tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+We are using django 1.4 so we need to load all the tests here
+if we want to split them in different modules.
+"""
+import unittest
+
+
+def suite():
+    return unittest.TestLoader().discover("core.tests", pattern="*.py")
--- core/tests/test_app_engine_example.py
+++ core/tests/test_app_engine_example.py
@@ -1,5 +1,3 @@
-import unittest
-
 from google.appengine.ext import ndb
 
 from ndbtestcase import AppEngineTestCase
@@ -9,14 +7,10 @@ class Thing(ndb.Model):
     stuff = ndb.StringProperty()
 
 
-class ATestCase(AppEngineTestCase):
+class ExampleTestCase(AppEngineTestCase):
     """Example App Engine testcase"""
 
     def test_thing_exists(self):
         self.thing = Thing(stuff="Hi")
         self.thing.put()
         self.assertEquals(Thing.query().count(), 1)
-
-
-if __name__ == '__main__':
-    unittest.main()
