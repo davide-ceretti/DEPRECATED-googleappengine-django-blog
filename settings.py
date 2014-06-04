2ae8c2cd490dc4dc0c98776a364f14dbb3b73e73
Davide Ceretti <dav.ceretti@gmail.com>
2014-06-04 01:18:06 +0100

    Fix settings.py

--- settings.py
+++ settings.py
@@ -39,27 +39,27 @@ SESSION_ENGINE = "appengine_sessions.backends.cached_db"
 # Uncomment these DB definitions to use Cloud SQL.
 # See: https://developers.google.com/cloud-sql/docs/django#development-settings
 #import os
-if (os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine') or
-   os.getenv('SETTINGS_MODE') == 'prod'):
-   # Running on production App Engine, so use a Google Cloud SQL database.
-   DATABASES = {
-       'default': {
-           'ENGINE': 'google.appengine.ext.django.backends.rdbms',
-           'INSTANCE': 'my_project:instance1',
-           'NAME': 'my_db',
-           }
-       }
-else:
-   # Running in development, so use a local MySQL database.
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.sqlite3',
-#            'USER': 'root',
-#            'PASSWORD': '',
-#            'HOST': 'localhost',
-           'NAME': 'my_db',
-           }
-       }
+# if (os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine') or
+#    os.getenv('SETTINGS_MODE') == 'prod'):
+#    # Running on production App Engine, so use a Google Cloud SQL database.
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'google.appengine.ext.django.backends.rdbms',
+#            'INSTANCE': 'my_project:instance1',
+#            'NAME': 'my_db',
+#            }
+#        }
+# else:
+#    # Running in development, so use a local MySQL database.
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+# #            'USER': 'root',
+# #            'PASSWORD': '',
+# #            'HOST': 'localhost',
+#            'NAME': 'my_db',
+#            }
+#        }
 
 
 
@@ -164,9 +164,9 @@ INSTALLED_APPS = (
     'core',
     'appengine_sessions',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
